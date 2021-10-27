bf2bb4cb7d486a4a801358bdc6ed65aec80f5502
itsjoseantonio <jos.valga@gmail.com>
2021-10-27 16:24:05 -0500

    darkmode

--- src/components/SwitchMode.js
+++ src/components/SwitchMode.js
@@ -1,15 +1,54 @@
+import { useState, useEffect } from 'react';
+
 // Components //
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 // Styles //
 import styles from '../Styles/components/SwitchMode.module.scss';
 
 const SwitchMode = () => {
+    const [darkMode, setDarkMode] = useState(
+        localStorage.getItem('theme') === 'dark' ? true : false
+    );
+
+    useEffect(() => {
+        if (localStorage.getItem('theme') === null) {
+            document
+                .getElementsByTagName('HTML')[0]
+                .setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }
+
+        document
+            .getElementsByTagName('HTML')[0]
+            .setAttribute('data-theme', localStorage.getItem('theme'));
+    }, []);
+
+    const toggleTheme = () => {
+        if (darkMode === false) {
+            localStorage.setItem('theme', 'dark');
+            document
+                .getElementsByTagName('HTML')[0]
+                .setAttribute('data-theme', localStorage.getItem('theme'));
+            setDarkMode(true);
+        } else {
+            localStorage.setItem('theme', 'light');
+            document
+                .getElementsByTagName('HTML')[0]
+                .setAttribute('data-theme', localStorage.getItem('theme'));
+            setDarkMode(false);
+        }
+    };
+
     return (
-        <span className={styles.switch}>
-            <FontAwesomeIcon icon={faMoon} />
-            Dark Mode
+        <span className={styles.switch} onClick={toggleTheme}>
+            {!darkMode ? (
+                <FontAwesomeIcon icon={faMoon} />
+            ) : (
+                <FontAwesomeIcon icon={faSun} />
+            )}
+            {!darkMode ? 'Dark' : 'Light'} Mode
         </span>
     );
 };
